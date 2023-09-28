efcb3a9b8fffa02376b41ceccf164029974fd7af
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-28 12:09:30 +0300

    fix  Remove OLDPWD and execute cd - test case (A.C)

--- main.c
+++ main.c
@@ -3,6 +3,9 @@ int Exit_fun(char *cmd, char **args, int *status);
 int positive_parseInt(char *str);
 int update_OLDPWD();
 void print_strn(char *str, int n);
+int handling_NULL_OLDPATH(char *PWD);
+int cd_to_OLD_PATH(char *PWD);
+int cd_Home_path(char *PWD);
 char *app_name;
 int counter = 1;
 /**
@@ -95,69 +98,16 @@ int builtIns(char *cmd, char **args, int *status)
 	else if (!strcmp("cd", cmd))
 	{
 
-		char *oldPWD_cpy;
 		char *PWD = (_getnEnv("PWD", 3));
 
 		if (PWD == NULL)
 			return (1);
 
 		if (args[1] == NULL)
-		{
-			char *home_dir = _getnEnv("HOME", 4);
-			char *home_dir_cpy = NULL;
-			char **home_dir_arr = NULL;
-
-			if (home_dir == NULL)
-				return (1);
-
-			home_dir_cpy = strdup(home_dir);
-			is_malloc_failed(home_dir_cpy);
-
-			home_dir_arr = tokenize_string(home_dir_cpy, "=");
-
-			is_malloc_failed(home_dir_arr);
-
-			update_OLDPWD(PWD);
-			change_dir(home_dir_arr[1]);
-
-			free(home_dir_cpy);
-			free_double_arr(&home_dir_arr);
-			return (1);
-		}
+			return cd_Home_path(PWD);
 
 		if (!strcmp("-", args[1]) || !strcmp("-\n", args[1]))
-		{
-			char *OLDPWD = (_getEnv("OLDPWD"));
-			char **arr;
-
-			if (OLDPWD == NULL)
-			{
-				char *PWD_cpy = malloc(strlen(PWD) + 2 * sizeof(char));
-
-				strcpy(PWD_cpy, PWD);
-
-				if (!PWD_cpy)
-					return (1);
-
-				strcat(PWD_cpy, "\n");
-				print_strn(PWD_cpy, 4);
-
-				free(PWD_cpy);
-				return (1);
-			}
-
-			oldPWD_cpy = strdup(OLDPWD);
-			arr = tokenize_string(oldPWD_cpy, "=");
-
-			printf("%s\n", arr[1]);
-
-			update_OLDPWD(PWD);
-			change_dir(arr[1]);
-
-			free_double_arr(&arr);
-			free(oldPWD_cpy);
-			return (1);
-		}
+			return cd_to_OLD_PATH(PWD);
 
 		update_OLDPWD(PWD);
 		change_dir(args[1]);
@@ -270,4 +220,64 @@ void print_strn(char *str, int n)
 
 	for (i = n; i < strlen(str); i++)
 		putchar(str[i]);
+}
+
+int handling_NULL_OLDPATH(char *PWD)
+{
+	char *PWD_cpy = malloc(strlen(PWD) + 2 * sizeof(char));
+
+	strcpy(PWD_cpy, PWD);
+
+	if (!PWD_cpy)
+		return (1);
+
+	strcat(PWD_cpy, "\n");
+	print_strn(PWD_cpy, 4);
+
+	free(PWD_cpy);
+	return (1);
+}
+int cd_to_OLD_PATH(char *PWD)
+{
+	char *OLDPWD = (_getEnv("OLDPWD"));
+	char **arr;
+	char *oldPWD_cpy;
+
+	if (OLDPWD == NULL)
+		return handling_NULL_OLDPATH(PWD);
+
+	oldPWD_cpy = strdup(OLDPWD);
+	arr = tokenize_string(oldPWD_cpy, "=");
+
+	printf("%s\n", arr[1]);
+
+	update_OLDPWD(PWD);
+	change_dir(arr[1]);
+
+	free_double_arr(&arr);
+	free(oldPWD_cpy);
+	return (1);
+}
+int cd_Home_path(char *PWD)
+{
+	char *home_dir = _getnEnv("HOME", 4);
+	char *home_dir_cpy = NULL;
+	char **home_dir_arr = NULL;
+
+	if (home_dir == NULL)
+		return (1);
+
+	home_dir_cpy = strdup(home_dir);
+	is_malloc_failed(home_dir_cpy);
+
+	home_dir_arr = tokenize_string(home_dir_cpy, "=");
+
+	is_malloc_failed(home_dir_arr);
+
+	update_OLDPWD(PWD);
+	change_dir(home_dir_arr[1]);
+
+	free(home_dir_cpy);
+	free_double_arr(&home_dir_arr);
+	return (1);
 }
