8c0d8088984764a4098b95feaf9f6c4794301069
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-28 10:55:44 +0300

    fix  Remove OLDPWD and execute cd - test case (A.C)

--- cd.c
+++ cd.c
@@ -3,8 +3,14 @@ int change_dir(char *path)
 {
 	char buff[100];
 
-	chdir(path);
+	;
+	if (chdir(path) == -1)
+	{
+		fprintf(stderr, "cd: no such file or directory: %s\n", path);
+		return (-1);
+	}
+
 	getcwd(buff, 100);
 	setenv("PWD", buff, 1);
-	return 0;
+	return (0);
 }
